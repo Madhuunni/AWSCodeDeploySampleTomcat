55475d95b830f70fd2e514eaaebed96e85afc88f
Madhuunni <88714502+Madhuunni@users.noreply.github.com>
2021-08-12 15:43:45 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+echo "Start"
 aws s3 cp s3://mpp-proj-artifacts/sample-demo-java-x11/LoginWebApp-1.war /tmp
 sudo mv /tmp/LoginWebApp-1.war /usr/share/tomcat/webapps/LoginWebApp-1.war
 sudo systemctl restart tomcat
